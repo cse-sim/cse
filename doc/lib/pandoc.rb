eba1b07d9efd49387c236e602a655159448bb298
Michael Patrick O'Keefe <michael.okeefe@bigladdersoftware.com>
2016-09-21 09:22:36 -0600

    Add copyright and license notices on helper scripts

--- doc/lib/pandoc.rb
+++ doc/lib/pandoc.rb
@@ -1,3 +1,6 @@
+# Copyright (c) 1997-2016 The CSE Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license
+# that can be found in the LICENSE file.
 require_relative('command')
 
 module Pandoc
--- doc/rakefile.rb
+++ doc/rakefile.rb
@@ -1,4 +1,8 @@
-# Build the Documents
+# Copyright (c) 1997-2016 The CSE Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license
+# that can be found in the LICENSE file.
+########################################
+# Build Documentation and Website
 require('fileutils')
 require('time')
 require('json')
