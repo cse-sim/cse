fdc914d63aef5a0f3a5001c87e99740d06bb4326
Michael Patrick O'Keefe <michael.okeefe@bigladdersoftware.com>
2017-02-24 10:20:36 -0700

    Assert that context variables don’t stay with the class
    A variable is only defined on the instance, not the class. Thus, the class definition stays pristine

--- doc/test/table_test.rb
+++ doc/test/table_test.rb
@@ -13,6 +13,14 @@ class TableTest < Minitest::Unit::TestCase
     assert_equal(t2.n, "b")
     assert_equal(t2.inspect, "c")
   end
+  def test_context_is_only_on_instance
+    t1 = Main.new(:template=>"<%= m %>", :context=>{"m"=>"a"})
+    t2 = Main.new(:template=>"<%= n %>", :context=>{"n"=>"a"})
+    assert(t1.respond_to?(:m))
+    assert(!t1.respond_to?(:n))
+    assert(!t2.respond_to?(:m))
+    assert(t2.respond_to?(:n))
+  end
   def test_render
     expected = "Hi a and b"
     actual = Main.new(:template=>"Hi <%= m %> and <%= n %>", :context=>{"m"=>"a", "n"=>"b"}).render
