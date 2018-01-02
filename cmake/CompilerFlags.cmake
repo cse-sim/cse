671802966d6855c544ca77a69f93847354afbfa7
Neal Kruis <neal.kruis@bigladdersoftware.com>
2018-01-02 15:37:14 -0700

    Sync release flags and linker flags.

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -10,6 +10,7 @@ if (MSVC AND NOT ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"))
   # '*' indicates CMake default option
   # '+' indicates default compiler behavior
 
+  ## Compiler flags
   set(CommonFlags
     /DWIN32     #*
   # /D_WINDOWS  #*
@@ -19,20 +20,30 @@ if (MSVC AND NOT ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"))
     /GR         #*Enable Run-Time Type Information TODO: keep?
     /EHsc       #*Specifies the model of exception handling (sc options).
     /nologo     # Suppresses display of sign-on banner.
-  # /fp:precise #+Specifies floating-point behavior.
+    /fp:precise #+Specifies floating-point behavior.
+    /fp:except- # Specifies floating-point behavior.
+    /arch:IA32  # Specifies the architecture for code generation (no special instructions).
   )
-
   string(REPLACE ";" " " CMAKE_CXX_FLAGS "${CommonFlags}")
 
   set(ReleaseFlags
-    /DNDEBUG    #*
+  # /DNDEBUG    #* TODO: Add back?
   # /MD         #*Creates a multithreaded DLL using MSVCRT.lib.
     /MT         # Creates a multithreaded executable file using LIBCMT.lib.
-    /O2         #*Creates fast code.
-    /Ob2        #*Controls inline expansion (level 2).
-    /Zi         #*Includes debug information in a program database compatible with Edit and Continue.
+    /GF         # Enables string pooling.
+    /GL         # Enables whole program optimization.
+    /Gy         # Enables function-level linking.
+  # /O2         #*Creates fast code.
+  # /Ob2        #*Controls inline expansion (level 2).
+    /Oi         # Generates intrinsic functions.
+    /Ot         # Favors fast code.
+    /Ox         # Uses maximum optimization (/Ob2gity /Gs).
+    /Oy         # Omits frame pointer (x86 only).
+    /Gd         # Uses the __cdecl calling convention (x86 only).
+    /Zi         #*Generates complete debugging information.
+    /GS-        # Buffers security check.
+    /MP         # Compiles multiple source files by using multiple processes.
   )
-
   string(REPLACE ";" " " CMAKE_CXX_FLAGS_RELEASE "${ReleaseFlags}")
 
   set(DebugFlags
@@ -45,7 +56,31 @@ if (MSVC AND NOT ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"))
     /RTC1       #*Enables run-time error checking.
     /Gm         # Enables minimal rebuild.
   )
-
   string(REPLACE ";" " " CMAKE_CXX_FLAGS_DEBUG "${DebugFlags}")
+
+  ## Linker flags
+  set(CommonLinkFlags
+    /NOLOGO     # Suppresses the startup banner.
+    /DYNAMICBASE#Specifies whether to generate an executable image that can be randomly rebased at load time by using the address space layout randomization (ASLR) feature.
+    /MACHINE:X86 # Specifies the target platform.
+  )
+  string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CommonLinkFlags}")
+
+  set(ReleaseLinkFlags
+    /LTCG       # Specifies link-time code generation.
+    /DEBUG	    # Creates debugging information.
+    /MAP        # Creates a mapfile.
+    /INCREMENTAL:NO # Controls incremental linking.
+  )
+  string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS_RELEASE "${ReleaseLinkFlags}")
+
+  set(DebugLinkFlags
+    /DEBUG	    # Creates debugging information.
+    /MAP        # Creates a mapfile.
+    /INCREMENTAL # Controls incremental linking.
+  )
+  string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS_DEBUG "${DebugLinkFlags}")
+
+
 
 endif()
