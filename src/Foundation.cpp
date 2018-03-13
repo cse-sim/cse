02045a0e293c7b703f9ceeafe2cfb466797de0b1
Neal Kruis <neal.kruis@bigladdersoftware.com>
2018-03-13 16:21:40 -0600

    Add initial foundation cpp file.

---
+++ src/Foundation.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 1997-2018 The CSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+///////////////////////////////////////////////////////////////////////////////
+// Foundation.cpp -- interface to Kiva
+///////////////////////////////////////////////////////////////////////////////
+
+#include "cnglob.h"
+#include "libkiva/Ground.hpp"
