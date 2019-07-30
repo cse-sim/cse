bc7e0a218b1b13bbc34f6aa4316a0628cebf2f73
Neal Kruis <neal.kruis@bigladdersoftware.com>
2019-07-30 12:17:22 -0600

    Switch from cmake external project to git submodules for dependencies.

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -2,7 +2,7 @@ message("Making build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
 message("Generating project files...")
-execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v141_xp
+execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v141_xp -A Win32
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
   RESULT_VARIABLE success
 )
