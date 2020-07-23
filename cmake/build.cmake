0a4e1eafed9545b4f131580a7473dad7457cfa3e
Neal Kruis <neal.kruis@bigladdersoftware.com>
2020-07-23 09:12:16 -0600

    Move CI to GitHub Actions (#102)
    * Initial attempt to set up GitHub actions.
    
    * Remove appveyor setup.
    
    * Move jobs into a single file.
    
    * Change shell for build.
    
    * Fix extra build and pandoc install.
    
    * Change pandoc install args.
    
    * Refresh environment.
    
    * Check pandoc version.
    
    * Change chocolatey install.
    
    * Refresh environment. Again.
    
    * Check PATH.
    
    * Use powershell environment variable.
    
    * Install pandoc for all users.
    
    * Add pandoc to PATH.
    
    * Use workflow command to add to PATH.
    
    * Combine into a single commit.
    
    * Add msbuild commands to PATH.
    
    * Set up LaTeX, and use MSVC developer commands.
    
    * Separate configure and build CMake scripts.
    
    * Separate out CI jobs.
    
    * Fix yml syntax.
    
    * Build wcmp for testing.
    
    * Fix wcmp build config.
    
    * Remove name from download path.
    
    * Install additional LaTeX packages.
    
    * Deploy on tag.
    
    * Add separate release workflow.
    
    * Fix workflow syntax.
    
    * Use different action for deploying gh-pages.
    
    * Fix gh-pages commit message.
    
    * Update badge.

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -1,16 +1,3 @@
-message("Making build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
-message("Generating project files...")
-execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v141_xp -A Win32
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
-  RESULT_VARIABLE success
-)
-if (NOT ${success} MATCHES "0")
-  message("Generation step failed.")
-  return()
-endif()
-
 message("Building CSE...")
 execute_process(COMMAND ${CMAKE_COMMAND}
   --build ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
