cc210dabe148aef381f4cf14d1463c726cbd8804
Charles Barnaby <chipbarnaby@gmail.com>
2021-05-17 14:14:19 -0400

    Initial ASHRAE 205 interface

---
+++ src/a205face.h
@@ -0,0 +1,10 @@
+// Copyright (c) 1997-2021 The CSE Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+// a205face.h -- defn and decls for interface to ASHRAE Std205
+//     equipment data representations
+
+
+
+// end a205face.h
