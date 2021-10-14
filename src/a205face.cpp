de45bb93e76d97da9a665f8ad6b6a720dcaf1303
Neal Kruis <neal.kruis@bigladdersoftware.com>
2021-10-14 16:57:19 -0600

    Start basic input structure.

--- src/a205face.cpp
+++ src/a205face.cpp
@@ -10,51 +10,5 @@
 
 
 
-#if 0
-// experimental code 9/2020 ?
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <istream>
-
-#include "json.hpp"
-
-using json = nlohmann::json;
-
-RC TestCBOR(
-	const char* filePath)
-{
-	RC rc = RCOK;
-
-	std::ifstream fileStream(filePath);
-
-	const char* ext = strpathparts(filePath, STRPPEXT);
-	bool isCBOR = stricmp(ext, ".cbor") == 0;
-
-	json x = 
-		isCBOR ? json::from_cbor( fileStream)
-			   : json::parse(fileStream);
-
-#if 0
-	MYLOADER L( x, otherstuff)
-
-	std::string rs_id = L.get("RS_ID", "?");
-
-	fluidType = L.getEnum("Fluid", enumTable)
-
-	timeStamp = L.getTimeStamp()
-
-	string z = x.value("name")
-#endif
-
-
-	for (auto it = x.begin(); it != x.end(); ++it)
-	{
-		std::cout << "key: " << it.key() << ", value:" << it.value() << '\n';
-	}
-
-	return rc;
-}
-#endif
 
 // a205face.cpp end
