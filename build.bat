07d193f079c8d2148201f324c3702b5e8c6f1697
Neal Kruis <neal.kruis@bigladdersoftware.com>
2021-11-24 08:35:49 -0700

    Update reference results.

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 cmake -P cmake/configure.cmake
+IF %ERRORLEVEL% NEQ 0 (
+    exit %ERRORLEVEL%
+)
 cmake -P cmake/build.cmake
+IF %ERRORLEVEL% NEQ 0 (
+    exit %ERRORLEVEL%
+)
 IF "%1"=="" (
   PAUSE
 )
