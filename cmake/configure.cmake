1eb1321af7f5b13742181420195c7a5bdc527a91
Neal Kruis <neal.kruis@bigladdersoftware.com>
2021-12-21 10:31:11 -0700

    Peg SDK version.

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -2,7 +2,7 @@ message("Making build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
 message("Generating project files...")
-execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142 -A Win32
+execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142 -A Win32 -DCMAKE_SYSTEM_VERSION=10.0.19041.0
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
   RESULT_VARIABLE success
 )
