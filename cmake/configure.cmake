485ae81619454264ce83d9e4616ebdfde919a568
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2022-04-12 13:36:29 -0600

    Update configure.

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -2,7 +2,6 @@ message("Making build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
 message("Generating project files...")
-# Note: Any change to CMAKE_SYSTEM_VERSION must also be reflected in the workflow yaml files [build-and-test, release].yml
 execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142,version=14.29.16.11 -A Win32 -DCMAKE_SYSTEM_VERSION=10.0.20348.0
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
   RESULT_VARIABLE success
