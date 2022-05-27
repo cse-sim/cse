8e6a99dbe1347b6daf00e8038dd4cb3f69a4da6d
Cesar Galan <galanc3.3@gmail.com>
2022-05-27 10:09:04 -0600

    Minor fix.

--- src/ashwat.cpp
+++ src/ashwat.cpp
@@ -1,4 +1,4 @@
-55// Copyright (c) 1997-2019 The CSE Authors. All rights reserved.
+// Copyright (c) 1997-2019 The CSE Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file.
 
