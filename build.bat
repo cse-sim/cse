35b2cc2c4d6b212bb47f08292a2dd6bd516aab7c
galanca <cesar.galan@colorado.edu>
2022-06-27 15:38:50 -0600

    Add option to build 64-bit.

--- build.bat
+++ build.bat
@@ -2,13 +2,13 @@
 if "%parent%"=="" set parent=%~0
 if "%console_mode%"=="" (set console_mode=1& for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set console_mode=0)
 
-cmake -P cmake/configure.cmake
+if "%1"=="/x64" (cmake -DX64=ON -P cmake/configure.cmake) ELSE (cmake -P cmake/configure.cmake)
 if %errorlevel% neq 0 (
   if "%parent%"=="%~0" ( if "%console_mode%"=="0" pause )
   exit /B %errorlevel%
 )
 
-cmake -P cmake/build.cmake
+if "%1"=="/x64" (cmake -DX64=ON -P cmake/build.cmake) ELSE (cmake -P cmake/build.cmake)
 if %errorlevel% neq 0 (
   if "%parent%"=="%~0" ( if "%console_mode%"=="0" pause )
   exit /B %errorlevel%
--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -1,25 +1,28 @@
-# Build 64-bit CSE
-message("Making build x64 directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
-file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build64)
-message("Generating x64 project files...")
-execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142,version=14.29.16.11 -A x64 -DCMAKE_SYSTEM_VERSION=10.0.20348.0
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build64
-  RESULT_VARIABLE success
-)
-if (NOT ${success} MATCHES "0")
-  message(FATAL_ERROR "Generation x64 step failed.")
-endif()
+if (X64)
+  # Build 64-bit CSE
+  message("Making x64 build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build64)
+  message("Generating x64 project files...")
+  execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142,version=14.29.16.11 -A x64 -DCMAKE_SYSTEM_VERSION=10.0.20348.0
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build64
+    RESULT_VARIABLE success
+  )
+  if (NOT ${success} MATCHES "0")
+    message(FATAL_ERROR "Generation x64 step failed.")
+  endif()
 
+else()
 # Build 32-bit CSE 
-#message("Making build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
-#file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
-#file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
-#message("Generating project files...")
-#execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142,version=14.29.16.11 -A Win32 -DCMAKE_SYSTEM_VERSION=10.0.20348.0
-#  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
-#  RESULT_VARIABLE success
-#)
-#if (NOT ${success} MATCHES "0")
-#  message(FATAL_ERROR "Generation step failed.")
-#endif()
+  message("Making x32 build directory: ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build")
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc)
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build)
+  message("Generating project files...")
+  execute_process(COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR} -T v142,version=14.29.16.11 -A Win32 -DCMAKE_SYSTEM_VERSION=10.0.20348.0
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/msvc/build
+    RESULT_VARIABLE success
+  )
+  if (NOT ${success} MATCHES "0")
+    message(FATAL_ERROR "Generation step failed.")
+  endif()
+endif()
