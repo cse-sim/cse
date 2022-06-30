e3610b2969b4d762a27e81745e8f824efe6efd60
galanca <cesar.galan@colorado.edu>
2022-06-30 10:28:39 -0600

    Add different compiler options between x64 and x32.

--- cmake/BuildFlags.cmake
+++ cmake/BuildFlags.cmake
@@ -9,19 +9,39 @@ if (MSVC AND NOT ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"))
   # Compiler flags #
   #================#
 
-  set(CompileCommon
-  #  /DWIN64     #*
-  # /D_WINDOWS  #*
-    /D_CONSOLE  #
-    /DINCNE     # CSE-specific
-    /W3         #*Warning level
-    /GR         #*Enable Run-Time Type Information TODO: keep?
-    /EHsc       #*Specifies the model of exception handling (sc options).
-    /nologo     # Suppresses display of sign-on banner.
-    /fp:precise #+Specifies floating-point behavior.
-    /fp:except- # Specifies floating-point behavior.
-  #  /arch:IA32  # Specifies the architecture for code generation (no special instructions).
-  )
+  if(USE_X64)
+    message("64 is working")
+    set(CompileCommon
+    #  /DWIN64     #*
+    # /D_WINDOWS  #*
+      /D_CONSOLE  #
+      /DINCNE     # CSE-specific
+      /W3         #*Warning level
+      /GR         #*Enable Run-Time Type Information TODO: keep?
+      /EHsc       #*Specifies the model of exception handling (sc options).
+      /nologo     # Suppresses display of sign-on banner.
+      /fp:precise #+Specifies floating-point behavior.
+      /fp:except- # Specifies floating-point behavior.
+    #  /arch:IA32  # Specifies the architecture for code generation (no special instructions).
+    )
+  else()
+    message("You are using 32")
+    set(CompileCommon
+      /DWIN32     #*
+    # /D_WINDOWS  #*
+      /D_CONSOLE  #
+      /DINCNE     # CSE-specific
+      /W3         #*Warning level
+      /GR         #*Enable Run-Time Type Information TODO: keep?
+      /EHsc       #*Specifies the model of exception handling (sc options).
+      /nologo     # Suppresses display of sign-on banner.
+      /fp:precise #+Specifies floating-point behavior.
+      /fp:except- # Specifies floating-point behavior.
+      /arch:IA32  # Specifies the architecture for code generation (no special instructions).
+    )
+  endif()
+
+
   string(REPLACE ";" " " CMAKE_CXX_FLAGS "${CompileCommon}")
 
   set(CompileRelease
@@ -59,11 +79,19 @@ if (MSVC AND NOT ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"))
   # Linker flags #
   #==============#
 
-  set(LinkCommon
-    /NOLOGO     # Suppresses the startup banner.
-    /DYNAMICBASE#Specifies whether to generate an executable image that can be randomly rebased at load time by using the address space layout randomization (ASLR) feature.
-    /MACHINE:X64 # Specifies the target platform.
-  )
+  if(USE_X64)
+    set(LinkCommon
+      /NOLOGO     # Suppresses the startup banner.
+      /DYNAMICBASE#Specifies whether to generate an executable image that can be randomly rebased at load time by using the address space layout randomization (ASLR) feature.
+      /MACHINE:X64 # Specifies the target platform.
+    )
+  else()
+    set(LinkCommon
+      /NOLOGO     # Suppresses the startup banner.
+      /DYNAMICBASE#Specifies whether to generate an executable image that can be randomly rebased at load time by using the address space layout randomization (ASLR) feature.
+      /MACHINE:X86 # Specifies the target platform.
+    )
+  endif()
   string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${LinkCommon}")
 
   set(LinkRelease
