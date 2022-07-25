8a916cb2a5284bfa78480d63ca700cb71540b43c
galanca <cesar.galan@colorado.edu>
2022-07-25 12:20:24 -0600

    Fix inline changes.

--- src/strpak.h
+++ src/strpak.h
@@ -150,8 +150,8 @@ int strReplace( char* str, const char* sOld, const char* sNew,
 char* stristr( const char* str1, const char* str2);
 BOOL strMatch( const char* s1, const char* s2);
 #ifndef _MSC_VER
-int _stricmp(const char* char1, const char* char2);
-int _strnicmp(const char* char1, const char* char2, size_t count);
+intline int _stricmp(const char* char1, const char* char2);
+intline int _strnicmp(const char* char1, const char* char2, size_t count);
 char* _strupr(char* stringMod);
 char* _strlwr(char* stringMod);
 #endif
