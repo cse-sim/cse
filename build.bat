5e96944ea7567a4f74493fcdf1d943e6d5a9b130
galanca <cesar.galan@colorado.edu>
2022-11-01 08:20:32 -0600

    Set input flag for build.bat.

--- build.bat
+++ build.bat
@@ -2,13 +2,16 @@
 if "%parent%"=="" set parent=%~0
 if "%console_mode%"=="" (set console_mode=1& for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set console_mode=0)
 
-cmake -P cmake/configure.cmake
+set archB="x32"
+if "%1"=="x64" (set archB="%1")
+
+cmake -DARCHITECTUREBUILD=%archB% -P cmake/configure.cmake
 if %errorlevel% neq 0 (
   if "%parent%"=="%~0" ( if "%console_mode%"=="0" pause )
   exit /B %errorlevel%
 )
 
-cmake -P cmake/build.cmake
+cmake -DARCHITECTUREBUILD=%archB% -P cmake/build.cmake
 if %errorlevel% neq 0 (
   if "%parent%"=="%~0" ( if "%console_mode%"=="0" pause )
   exit /B %errorlevel%
