b2a6713835b1f6e8909def42b8834e7c8e871d83
Neal Kruis <neal.kruis@bigladdersoftware.com>
2022-11-03 14:11:34 -0600

    Fix build defines.

--- src/cnglob.h
+++ src/cnglob.h
@@ -19,15 +19,31 @@
 // CSE_ARCH_64 build targeting a 64-bit application architecture
 
 // #defines for different operating systems (https://sourceforge.net/p/predef/wiki/OperatingSystems/)
-#define CSE_OS_WINDOWS _WIN32 // _WIN32 Defined for both windows 32-bit and windows 64-bit environments 1
-#define CSE_OS_MACOS __APPLE__
-#define CSE_OS_LINUX __linux__  // May need to be more specific later (e.g., __GNU__)
+#ifdef _WIN32 // _WIN32 Defined for both windows 32-bit and windows 64-bit environments 1
+  #define CSE_OS_WINDOWS
+#endif
+#ifdef __APPLE__
+  #define CSE_OS_MACOS
+#endif
+#ifdef __linux__ // May need to be more specific later (e.g., __GNU__)
+  #define CSE_OS_LINUX
+#endif
 
 // #defines for different compilers (https://sourceforge.net/p/predef/wiki/Compilers/)
-#define CSE_COMPILER_MSVC _MSC_VER
-#define CSE_COMPILER_GCC __GNUC__ && !__clang__ // __GNUC__ is also defined in clang
-#define CSE_COMPILER_CLANG __clang__
-//#define CSE_COMPILER_APPLECLANG __clang__ && __apple_build_version__ // (unsure if we'll need this distinction)
+#ifdef _MSC_VER
+  #define CSE_COMPILER_MSVC
+#endif
+#ifdef __GNUC__
+  #ifndef __clang__  // __GNUC__ is also defined in clang
+    #define CSE_COMPILER_GCC
+  #endif
+#endif
+#ifdef __clang__
+  #define CSE_COMPILER_CLANG
+  #ifdef __apple_build_version__
+    #define CSE_COMPILER_APPLECLANG // (unsure if we'll need this distinction)
+  #endif
+#endif
 
 //--- Options in cndefns.h (eg for use in cnrecs.def), now #included below in this file ---
 //
