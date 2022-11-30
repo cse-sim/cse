bcd004db8cc9ad881403ffa280865a72083814f9
galanca <cesar.galan@colorado.edu>
2022-11-30 15:45:38 -0700

    Fix path.

--- test/error_comp/error_comp.cpp
+++ test/error_comp/error_comp.cpp
@@ -5,7 +5,7 @@ int main( int argc, char **argv)
 {
     std::fstream error_file;
     std::string file_content, file_name{ argv[1] }, error_code{argv[2]};
-    error_file.open("D:\code\cse\test\error_handling\no_error.err", std::fstream::in);
+    error_file.open(file_name, std::fstream::in);
     if(!error_file && error_file.is_open()) {
         error_file >> file_content;
         size_t loc = file_content.find(error_code);
