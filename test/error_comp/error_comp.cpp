4b636ac57192a04b4429fc0096d9b77afb89ff80
galanca <cesar.galan@colorado.edu>
2022-11-30 15:32:03 -0700

    Add map to contain file and error code.

--- test/error_comp/error_comp.cpp
+++ test/error_comp/error_comp.cpp
@@ -1,18 +1,17 @@
-#include <regex>
 #include <iostream>
 #include <fstream>
 
 int main( int argc, char **argv)
 {
-    std::ifstream ifs(argv[0]);
-    std::regex genex{"\\bP0003\\b"};
-    std::string line;
-
-    while( std::getline(ifs, line ))
-    {
-        std::cout << line;
-        if ( std::regex_search( line, genex) )
+    std::fstream error_file;
+    std::string file_content, file_name{ argv[1] }, error_code{argv[2]};
+    error_file.open("D:\code\cse\test\error_handling\no_error.err", std::fstream::in);
+    if(!error_file && error_file.is_open()) {
+        error_file >> file_content;
+        size_t loc = file_content.find(error_code);
+        if (loc != file_content.length()) {
             return 0;
+        }
     }
     return -1;
 }
