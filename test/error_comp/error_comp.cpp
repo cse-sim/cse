a55a115166f5974371736be7179b3ea6d9434f6d
galanca <cesar.galan@colorado.edu>
2022-12-02 11:21:34 -0700

    Fix error_comp.

--- test/error_comp/error_comp.cpp
+++ test/error_comp/error_comp.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main( int argc, char **argv)
 {
-    std::fstream error_file;
-    std::string file_content, file_name{ argv[1] }, error_code{argv[2]};
-    error_file.open(file_name, std::fstream::in);
-    if(!error_file && error_file.is_open()) {
-        error_file >> file_content;
-        size_t loc = file_content.find(error_code);
-        if (loc != file_content.length()) {
-            return 0;
+    std::string file_name{ argv[1] }, error_code{ argv[2] };
+    std::ifstream error_file (file_name);
+
+    if (error_file.is_open() && !error_file.eof()) {
+        std::string line;
+        size_t loc;
+        while (std::getline(error_file, line)) {
+            if (line.find(error_code) != -1) {
+                return 0; // Error code found
+            }
         }
     }
+    // Could not open the file or error code not found
     return -1;
 }
