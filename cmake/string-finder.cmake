4a68d0937ddf1cee1ef48e5f2b9cb0672ec4dedb
galanca <cesar.galan@colorado.edu>
2022-12-05 11:18:21 -0700

    Eliminate quotes from error_string variable.

--- cmake/string-finder.cmake
+++ cmake/string-finder.cmake
@@ -1,4 +1,9 @@
 file(READ ${file} file_content)
 
 string(FIND ${file_content} ${error_string} pos)
-message(${pos})
+
+if (${pos} LESS 0)
+    message(FATAL_ERROR "${pos}")
+else()
+    message(${pos})
+endif()
