96e1e02822ae174405952fc6528646236b0ad516
galanca <cesar.galan@colorado.edu>
2022-12-22 10:26:01 -0700

    Improve error message.

--- cmake/string-finder.cmake
+++ cmake/string-finder.cmake
@@ -1,9 +1,8 @@
+# string-finder: Returns an error if string is not found in the given file. Otherwise exits gracefully.
 file(READ ${file} file_content)
 
-string(FIND ${file_content} ${error_string} content_location)
+string(FIND ${file_content} ${string} content_location)
 
 if (${content_location} LESS 0)
-    message(FATAL_ERROR "Content not found in the file, ${content_location}")
-else()
-    message(${content_location})
+    message(FATAL_ERROR "STRING-FINDER ERROR: String, \"${string}\", not found in the file, \"${file}\".")
 endif()
