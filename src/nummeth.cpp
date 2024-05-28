e691844f200209ca756d46dba37d20ef217500bb
Neal Kruis <neal.kruis@bigladdersoftware.com>
2024-05-28 14:28:44 -0600

    Clean up secant failure formatting.

--- src/nummeth.cpp
+++ src/nummeth.cpp
@@ -265,9 +265,8 @@ int secant( // screen secant success (see above); report calculation if failure
   x1 = x1_prev; // secant (above) failed. Restore values and repeat with output
   f1 = f1_prev;
 
-  auto msg = fmt::format("secant failed; target {:g}\n", f);
-  msg += fmt::format("initial: x1 = {:g}, x2 = {:g}, f1 = {:g}, f2 = {:g}\n", x1, x2, f1, f2);
- 
+  auto msg = fmt::format("Secant failed to converge for target f = {:g}.\n", f);
+
   double fHi = f + eps;
   double fLo = f - (epsLo >= 0. ? epsLo : eps);
 
@@ -290,8 +289,8 @@ int secant( // screen secant success (see above); report calculation if failure
   int i;
   for (i = 0; ++i < 20;) // iterate to refine solution
   {
-    msg += fmt::format("begin iter {: } ----------\n", i);
-    msg += fmt::format("before: x1 = {:g}, x2 = {:g}, f1 = {:g}, f2 = {:g}\n", x1, x2, f1, f2);
+
+    msg += fmt::format("    Iteration {}: x1 = {:g}, x2 = {:g}, f1 = {:g}, f2 = {:g}\n", i, x1, x2, f1, f2);
 
     if (f1 <= fHi && f1 >= fLo) {
       i = 0; // success
@@ -303,11 +302,11 @@ int secant( // screen secant success (see above); report calculation if failure
     if (fabs(f1 - f2) < 1.e-20) // if slope is 0
     {
       i = -i; // tell caller
+      msg += "    Zero-slope detected.\n";
       break;
     }
 
     double xN = x1 + (x2 - x1) * (f - f1) / (f2 - f1);
-    msg += fmt::format("xN = {}\n", xN);
 
     // secant method: new guess assuming local linearity.
     x2 = x1; // replace older point
@@ -315,9 +314,13 @@ int secant( // screen secant success (see above); report calculation if failure
     x1 = xN;
     f1 = (*pFunc)(pO, x1); // new value
 
-    msg += fmt::format("after: x1 = {:g}, x2 = {:g}, f1 = {:g}, f2 = {:g}\n", x1, x2, f1, f2);
+
   }
  
+  if (i > 0) {
+    msg += "    Maximum iterations hit.\n";
+  }
+
   warn(msg.c_str());
   return i;
 } // ::secant
