45cf671b4fb70c479c4e7256a13af555cd579eed
Chip Barnaby <chipbarnaby@gmail.com>
2024-12-26 17:12:49 -0500

    Changed SI isWord to bool isWord

--- src/cuparsex.h
+++ src/cuparsex.h
@@ -27,7 +27,7 @@ extern SI nextPrec;		// "prec" of ungotten (ie next) token, only valid after exp
 //extern OPTBL * opp;		// ptr to opTbl entry for token
 extern const char * ttTx;	// saveable ptr to static token descriptive text (opp->tx) for errMsgs.  cul.cpp uses.
 // extern void * stbk; 		// symbol table value ptr, set by toke() for already-decl identifiers, type varies...
-extern SI isWord;  		// non-0 if word: reserved, defined, or CUTID.
+extern bool isWord;  		// true iff word: reserved, defined, or CUTID.
 
 /*--- CURRENT EXPRESSION INFO, exOrk to expr and callees, incl cuprobe.cpp. */
 extern USI evfOk;  	// evaluation frequencies allowed bits for current expression,
