f78c21de7d93f09cb598e073957907bc26009816
Matthew Brandt <matthew.brandt@bigladdersoftware.com>
2025-03-03 12:58:22 -0700

    Avoid adding sort buttons for tables with only two rows.

--- doc/src/docs/assets/javascript/extra_js.js
+++ doc/src/docs/assets/javascript/extra_js.js
@@ -1,6 +1,11 @@
 document$.subscribe(function () {
   var tables = document.querySelectorAll("article table:not([class])");
   tables.forEach(function (table) {
+    // The only way to have only two rows is to have either:
+    // - a header with one row (no need for sorting)
+    // - no header with two rows (again, no need for soring, since header is hidden)
+    if (table.querySelectorAll('tr').length === 2) return;
+
     new Tablesort(table);
   });
 });
